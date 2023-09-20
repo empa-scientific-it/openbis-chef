1e211116463d9bc2af815c82f42bb3807ab0a368
Simone Baffelli <simone.baffelli@empa.ch>
2023-09-20 10:36:54 +0200

    Added searching for samples.

--- services/app/src/metagraph/components/Select.tsx
+++ services/app/src/metagraph/components/Select.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext, useMemo } from "react";
 import { AuthContext } from "@src/openbis/AuthContext";
 import { OperationContext } from "../OperationContext";
-import { Experiment } from "@src/types/openbis";
 import {
   ExperimentFetchOptions,
   ExperimentSearchCriteria,
@@ -10,10 +9,31 @@ import {
   Sample,
   SampleSearchCriteria,
   CodeSearchCriteria,
+  Experiment,
+  AnyPropertySearchCriteria,
+  SizeSearchCriteria,
+  AnyStringPropertySearchCriteria,
+  SampleSortOptions,
+  Sorting,
 } from "@src/openbis/dto";
 import { MetagraphComponentProps } from "@src/metagraph/metagraph";
 import SampleEntry from "@src/openbis/components/SampleEntry";
 import "./Select.css";
+import { on } from "process";
+import { OpenBIS } from "@src/types/openbis";
+import { Facade } from "@src/openbis/api";
+import { as } from "fp-ts/lib/Option";
+
+function sampleFetchOptionsComplete() {
+  const sfo = new SampleFetchOptions();
+  const sto: typeof SampleTypeFetchOptions = new SampleTypeFetchOptions();
+  const sso: typeof SampleFetchOptions = new SampleFetchOptions();
+  sto.withPropertyAssignments().withPropertyType().withVocabulary();
+  sso.withProperties();
+  sso.withTypeUsing(sto);
+  sfo.withProperties();
+  return sfo;
+}
 
 const SampleSelector = ({
   samples,
@@ -32,7 +52,12 @@ const SampleSelector = ({
       {/* <div>Experiment: {experiment.identifier.identifier}</div> */}
       <label htmlFor="select-sample">Select sample:</label>
       {samples !== undefined ? (
-        <select id="select-sample" className="select-dropdown" value={selectedValue} onChange={localOnSelect}>
+        <select
+          id="select-sample"
+          className="select-dropdown"
+          value={selectedValue}
+          onChange={localOnSelect}
+        >
           {samples?.map((sample) => (
             <option
               key={sample.identifier.identifier}
@@ -49,40 +74,140 @@ const SampleSelector = ({
   );
 };
 
+const SampleTable = ({
+  samples,
+  onSelectedSample,
+  onSort,
+}: {
+  samples: (typeof Sample)[];
+  onSelectedSample: (s: typeof Sample) => void;
+  onSort: (s: { column: string; direction: "asc" | "desc" }) => void;
+}) => {
+  const props =
+    [...new Set(samples?.flatMap((sample) => Object.keys(sample?.properties ?? {})))] ??
+    [];
+  const [sortColumn, setSortColumn] = useState(""); // Track the currently sorted column
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc"); // Track sorting direction
+
+  const handleColumnHeaderClick = (column: string) => {
+    // If the column is already sorted, reverse the direction, otherwise set the column and direction
+    if (column === sortColumn) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortColumn(column);
+      setSortDirection("asc");
+    }
+  };
+
+  useEffect(() => {
+    onSort({ column: sortColumn, direction: sortDirection });
+  }, [sortColumn, sortDirection]);
+
+  // Function to render the sort indicator based on the current sorting state
+  const renderSortIndicator = (column: string) => {
+    if (sortColumn === column) {
+      if (sortDirection === "asc") {
+        return <span>&uarr;</span>; // Up arrow for ascending
+      } else {
+        return <span>&darr;</span>; // Down arrow for descending
+      }
+    }
+    return null;
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th onClick={() => handleColumnHeaderClick("Sample")}>
+            Sample {renderSortIndicator("Sample")}
+          </th>
+          <th onClick={() => handleColumnHeaderClick("PermID")}>
+            PermID {renderSortIndicator("PermID")}
+          </th>
+          <th onClick={() => handleColumnHeaderClick("Identifier")}>
+            Identifier {renderSortIndicator("Identifier")}
+          </th>
+          {props?.map((prop) => (
+            <th onClick={() => handleColumnHeaderClick(prop)}>
+              {prop} {renderSortIndicator(prop)}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {samples?.map((sample) => (
+          <tr key={sample.identifier.identifier} onClick={() => onSelectedSample(sample)}>
+            <td>{sample.code}</td>
+            <td>{sample.permId.permId}</td>
+            {props.map((prop) =>
+              sample.properties[prop] ? <td>{sample.properties[prop]}</td> : <td></td>
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+function createSearchCriteria(
+  collection: string,
+  objectType: string,
+  searchTerm: string
+) {
+  const ssc = new SampleSearchCriteria();
+  ssc.withExperiment().withIdentifier().thatEquals(collection);
+  ssc.withType().withCode().thatEquals(objectType);
+
+  ssc.withAnyProperty().thatContains(searchTerm);
+
+  console.log(ssc);
+  return ssc;
+}
+
+async function performSearch(
+  collection: string,
+  objectType: string,
+  searchTerm: string,
+  sfo: typeof SampleFetchOptions,
+  service: Facade
+) {
+  const ssc = createSearchCriteria(collection, objectType, searchTerm);
+  const result = await service.searchSamples(ssc, sfo);
+  return result;
+}
+
 const Select = () => {
   const workflowOperations = useContext(OperationContext);
   const { loggedIn, service } = useContext(AuthContext);
   const [samples, setSamples] = useState(null as (typeof Sample)[]);
   const [currentSample, setSample] = useState(null as typeof Sample);
   const currentCollection = workflowOperations.currentOperation.collectionIdentifier;
-
+  const [fetchOptions, setFetchOptions] = useState<typeof SampleFetchOptions>(
+    sampleFetchOptionsComplete()
+  ); // Track the currently sorted column
+  const [searchTerm, setSearchTerm] = useState("");
+  //Use one function to generate the fetch options
   useEffect(() => {
     if (loggedIn) {
       console.log(currentCollection);
       //Perform the search for all the objects in the experiment/collection
-      const ssc = new SampleSearchCriteria();
-      ssc
-        .withExperiment()
-        .withIdentifier()
-        .thatEquals(workflowOperations.currentOperation.collectionIdentifier);
-      ssc
-        .withType()
-        .withCode()
-        .thatEquals(workflowOperations.currentOperation.objectType);
-      const sfo = new SampleFetchOptions();
-      const sto: typeof SampleTypeFetchOptions = new SampleTypeFetchOptions();
-      const sso: typeof SampleFetchOptions = new SampleFetchOptions();
-      sto.withPropertyAssignments().withPropertyType();
-      sso.withProperties();
-      sso.withTypeUsing(sto);
-
-      service.searchSamples(ssc, sfo).then((res) => {
+      performSearch(
+        currentCollection,
+        workflowOperations.currentOperation.objectType,
+        searchTerm,
+        fetchOptions,
+        service
+      ).then((res) => {
         if (res.totalCount > 0) {
+          console.log(res);
           setSamples(() => res.objects);
+        } else {
+          setSamples(() => []);
         }
       });
     }
-  }, [loggedIn, currentCollection]);
+  }, [loggedIn, currentCollection, fetchOptions, searchTerm]);
 
   const handleSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
@@ -94,17 +219,45 @@ const Select = () => {
     workflowOperations.setIdentifier(foundSample?.identifier.identifier);
   };
 
-  const ui = useMemo(() => {
-    return <SampleSelector samples={samples} onSelect={handleSelection} />;
-  }, [samples]);
+  const startSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const searchTerm = event.target.value;
+    setSearchTerm(() => searchTerm);
+  };
+
+  const handleSort = (s: { column: string; direction: "asc" | "desc" }) => {
+    const sfo = sampleFetchOptionsComplete();
+    const sb = new SampleSortOptions();
+    const so = sb.property(s.column);
+    if (s.direction === "desc") {
+      so.asc = false;
+    }
+    console.log(s.direction, sb);
+    sfo.sort = sb;
+
+    setFetchOptions(sfo);
+  };
 
   return (
     <div>
       <main>
-        {ui}
+        <div>
+          <h3>Search objects</h3>
+          <label htmlFor="property-search">Search any properties:</label>
+          <input
+            onChange={startSearch}
+            id="property-search"
+            aria-label="Search in object properties"
+          ></input>
+        </div>
+        <SampleTable
+          samples={samples}
+          onSelectedSample={handleSelection}
+          onSort={handleSort}
+        />
         {/* <h3>Selected sample</h3> */}
-        <h3>{currentSample?.identifier?.identifier}</h3>
-        {currentSample !== null ? <SampleEntry sample={currentSample} /> : null}
+        {/* <h3>{currentSample?.identifier?.identifier}</h3>
+        {currentSample !== null ? <SampleEntry sample={currentSample} /> : null} */}
       </main>
     </div>
   );
---
+++ services/app/src/metagraph/components/WorkflowSelection.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Metagraph } from "../metagraph";
+import WorkflowEditor from "./WorkflowEditor";
+import "./Workflow.css";
+import "./WorkflowSelection.css";
+
+function WorkflowSelection({
+  workflows,
+  onSelect,
+  handleNewMetagraph,
+  currentWofklow,
+}: {
+  workflows: Metagraph[];
+  onSelect: (selectedWorkflow: Metagraph) => void;
+  handleNewMetagraph: (mg: Metagraph) => void;
+  currentWofklow: Metagraph;
+}) {
+  const [showNewWorkflow, setShowNewWorkflow] = useState(false);
+  const [selected, setSelected] = useState(currentWofklow);
+
+  const handleWorkflowSelect = (workflow: Metagraph) => {
+    setSelected(currentWofklow);
+    onSelect(workflow);
+  };
+
+  const handleNew = (mg: Metagraph) => {
+    console.log("closing");
+    console.log(mg)
+    setShowNewWorkflow((old) => !old);
+    handleNewMetagraph(mg);
+  };
+
+  const handleEdit = () => {
+    console.log("editing");
+    setShowNewWorkflow((old) => !old);
+  };
+
+  const handleCopy = () => {
+    console.log("copying");
+    handleNewMetagraph({ ...structuredClone(selected), name: selected.name + ` copy ${Date()}` });
+  };
+
+  return (
+    <div>
+      <h3>Available workflows:</h3>
+      <div className="workflow-actions-buttons">
+        <button onClick={handleEdit}>Edit workflow</button>
+        <button onClick={handleCopy}>Copy</button>
+        <button className="workflow-selection-item" onClick={handleNew}>
+          Add workflow
+        </button>
+      </div>
+      <table className="workflow-list">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workflows.map((workflow) => (
+            <tr
+              onClick={() => handleWorkflowSelect(workflow)}
+              className={
+                "workflow-selection-item" +
+                (selected.name === workflow.name
+                  ? " workflow-selection-item-selected"
+                  : "")
+              }
+            >
+              <td>{workflow.name}</td>
+              {/* <td>
+                    {workflow.name === selected ? <div className="workflow-selection-item-selected-indicator"></div> : <div className="workflow-selection-item-unselected-indicator"></div>}
+                </td> */}
+              <td>{workflow.description}</td>
+              {/* <button
+                  key={workflow.name}
+                  id={workflow.name}
+                  onClick={() => handleWorkflowSelect(workflow)}
+
+                >
+                  {workflow.name}
+                </button> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <WorkflowEditor
+        initialValue={selected}
+        handleNewMetagraph={handleNewMetagraph}
+        isOpen={showNewWorkflow}
+        handleClose={handleNew}
+      />
+    </div>
+  );
+}
+
+export default WorkflowSelection;
