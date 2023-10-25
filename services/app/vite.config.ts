35ce8e50dd02cfc7a9147b6e6f7fa82834e9ba15
Simone Baffelli <simone.baffelli@empa.ch>
2023-10-25 11:41:53 +0200

    Added development on container.

--- services/app/vite.config.ts
+++ services/app/vite.config.ts
@@ -1,52 +1,64 @@
-import { defineConfig } from "vitest/config";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-console.log("Running vite.config.ts");
-export default defineConfig({
-  plugins: [react(), basicSsl()],
-  base: "./",
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".jsx"],
-    alias: {
-      "@src": path.resolve(__dirname, "./src"),
-      v3api: path.resolve(__dirname, "./lib/v3api/js"),
+import { loadEnv, defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(env)
+  return {
+    plugins: [react(), basicSsl()],
+    base: "./",
+    envDir: "./",
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".jsx"],
+      alias: {
+        "@src": path.resolve(__dirname, "./src"),
+        v3api: path.resolve(__dirname, "./lib/v3api/js"),
+      },
     },
-  },
-  server: {
-    origin: "https://localhost:5173/",
-    cors: {
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      origin: "*",
-      credentials: true,
-      allowedHeaders: ["Content-Type", "Authorization"],
-      exposedHeaders: ["Content-Type", "Authorization"],
-    },
-    proxy: {
-      configure: {
-        configure(proxy, options) {
-          console.log("configure", proxy, options);
-          proxy.on("proxyReq", (proxyReq, req, res, options) => {
-            console.error("proxyReq", proxyReq, req, res, options);
-          });
+    server: {
+      origin: "https://localhost:5173/",
+      cors: {
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        origin: "*",
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+        exposedHeaders: ["Content-Type", "Authorization"],
+      },
+      proxy: {
+        configure: {
+          configure(proxy, options) {
+            console.log("configure", proxy, options);
+            proxy.on("proxyReq", (proxyReq, req, res, options) => {
+              console.error("proxyReq", proxyReq, req, res, options);
+            });
+          },
+        },
+        "/local/": {
+          //You can use https://sgl01646:8443/ for remote development with the instance in SG. You can also use https://openbis-empa-lab402.ethz.ch/openbis/webapp/eln-lims/ but be careful with creating objects
+          target: `${env.VITE_OPENBIS_HOST}`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => {
+            const newPath = path.replace("/local", "");
+            console.log(newPath);
+            return newPath;
+          },
+        },
+        "^/openbis-empa-(w+)/": {
+          target: "https://openbis-empa-lab402.ethz.ch/",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => {
+            console.log(path);
+            return path.replace(/^\/openbis-empa-(\w+)/, "/$1");
+          },
         },
       },
-      "/local/": {
-        //You can use https://sgl01646:8443/ for remote development with the instance in SG. You can also use https://openbis-empa-lab402.ethz.ch/openbis/webapp/eln-lims/ but be careful with creating objects
-        target: "https://localhost:8445/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => {const newPath =  path.replace("/local", ""); console.log(newPath); return newPath},
-      },
-      "^/openbis-empa-(\w+)/": {
-        target: "https://openbis-empa-lab402.ethz.ch/",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => {console.log(path); return path.replace(/^\/openbis-empa-(\w+)/, "/$1")},
-      },
     },
-  },
-  test: {
-    environment: "jsdom",
-  },
+    test: {
+      environment: "jsdom",
+    },
+  };
 });
